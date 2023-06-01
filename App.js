b4ae78456f1b20d4f25e10817f56b8ac234ccdaa
edsontecjr <edson.tec.jr@gmail.com>
2023-06-01 00:46:32 +0000

    finalizando appComb

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Image } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [gasPrice, setGasPrice] = useState('');
+  const [ethanolPrice, setEthanolPrice] = useState('');
+  const [result, setResult] = useState('');
+
+  const comparePrices = () => {
+    const gasValue = parseFloat(gasPrice);
+    const ethanolValue = parseFloat(ethanolPrice);
+    const ethanolGasRatio = ethanolValue / gasValue;
+
+    if (ethanolGasRatio < 0.7) {
+      setResult('A melhor escolha é comprar álcool.');
+    } else {
+      setResult('A melhor escolha é comprar gasolina.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/down.jpg')} style={styles.image} />
+      <Text style={styles.label}>Preço da GASOLINA:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={gasPrice}
+        onChangeText={setGasPrice}
+      />
+
+      <Text style={styles.label}>Preço do ÁLCOOL:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={ethanolPrice}
+        onChangeText={setEthanolPrice}
+      />
+
+      <Button title="Comparar Preços" onPress={comparePrices} />
+
+      <Text style={styles.result}>{result}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
+    backgroundColor: 'lightgrey',
+     maxWidth: 400,
+    marginTop: 0,
+    marginBottom: 0,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    alignSelf: 'center',
+    marginBottom: 16,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 8,
+    color: 'white',
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 16,
+    paddingHorizontal: 8,
+    backgroundColor: 'grey',
+    borderRadius: 8,
+  },
+  result: {
+    fontSize: 18,
+    marginTop: 16,
+    color: 'darkpink',
   },
 });
+
+export default App
